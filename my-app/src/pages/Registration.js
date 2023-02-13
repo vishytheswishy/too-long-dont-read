3e77765f1cd84fb14de1b3ccd2509665efc0460a
Vishaal Yalamanchali <vishaal.yalaman@gmail.com>
2023-02-12 23:13:24 -0600

    create terms and conditions

--- my-app/src/pages/Registration.js
+++ my-app/src/pages/Registration.js
@@ -1,7 +1,34 @@
 import "./App.css";
+import Modal from "./components/Modal";
 import Navbar from "./components/Navbar.js";
 
 const Registration = () => {
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function () {
+    modal.style.display = "block";
+    console.log("this works");
+  };
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
   return (
     <div class="tldr">
       <Navbar />
@@ -46,17 +73,26 @@ const Registration = () => {
             </div>
           </nav>
           <div class="field">
-              <div class="control">
-                <label class="checkbox">
-                  <input type="checkbox" /> I agree to the{" "}
-                  <a href="#">terms and conditions</a>
-                </label>
-              </div>
+            <div class="control">
+              <label class="checkbox">
+                <input type="checkbox" /> I agree to the{" "}
+                <button id="myBtn">terms and conditions</button>
+              </label>
             </div>
+          </div>
+          <div id="myModal" class="modal">
+            <div class="modal-content">
+              <span class="close">&times;</span>
+              <p class= "blk"> <b>Terms and Conditions </b></p>
+              <Modal/>
+            </div>
+          </div>
           <div class="columns">
             <div class="column">
               <div class="control column">
-                <a href="/success" class="blk button is-primary ">Submit your stock picks 🚀</a>
+                <a href="/success" class="blk button is-primary ">
+                  Submit your stock picks 🚀
+                </a>
               </div>
             </div>
           </div>
